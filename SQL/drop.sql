1f6cf7a52d5297da11ee15cb6b070daade0e96cc
sampathBhat <sampaths388@gmail.com>
2019-02-09 21:00:39 -0800

    drop and readme

---
+++ SQL/drop.sql
@@ -0,0 +1,14 @@
+DROP VIEW highrating;
+DROP VIEW lowrating;
+DROP VIEW noFlop;
+
+DROP TABLE Reviews;
+DROP TABLE Roles;
+DROP TABLE genres;
+
+DROP TABLE actorList;
+
+DROP TABLE movie;
+DROP TABLE IMDB_user;
+DROP TABLE person;
+
